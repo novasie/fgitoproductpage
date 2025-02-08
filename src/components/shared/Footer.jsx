b988134521731691bd97193d5e5b485f1274d195
Dharmvir Dharmacharya <ddharmacharya22@gmail.com>
2025-02-09 03:46:27 +0530

    payment sucess and failure page done

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,10 +2,18 @@ import Image from "next/image";
 import React from "react";
 
 const Footer = () => {
-  const HomechefUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdoQDcz8AQowT3mQVkrpIelZGqTJRV6ecBx-zA4SiUZk5wsaA/viewform";
+  const HomechefUrl =
+    "https://docs.google.com/forms/d/e/1FAIpQLSdoQDcz8AQowT3mQVkrpIelZGqTJRV6ecBx-zA4SiUZk5wsaA/viewform";
   return (
     <>
-      <div style={{ backgroundImage: "url('/img/homeBannerBg.svg')", backgroundSize: 'cover', backgroundPosition: 'center' }} className=" w-full px-[2.7rem] py-[2rem] lg:py-[3rem]">
+      <div
+        style={{
+          backgroundImage: "url('/img/homeBannerBg.svg')",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className=" w-full px-[2.7rem] py-[2rem] lg:py-[3rem]"
+      >
         <div className="flex flex-col w-full  gap-8 md:gap-0 md:flex-row justify-between items-center md:items-start md:relative">
           <div className="flex flex-col items-center  gap-2 md:items-start w-full md:w-[40%]">
             <Image src={"/logo.png"} width={150} height={150} />
@@ -13,7 +21,9 @@ const Footer = () => {
               Fg<span className="text-theme-red-100 lowercase">i</span>to
             </div>
             <div className="text-theme-black max-w-[20rem] md:text-[1rem] leading-[20px] font-semibold text-justify">
-            Busy lifestyles often lead to unhealthy food choices. FGITO offers a permanent solution, ensuring you get nutritious, home-cooked meals even on your busiest days.
+              Busy lifestyles often lead to unhealthy food choices. FGITO offers
+              a permanent solution, ensuring you get nutritious, home-cooked
+              meals even on your busiest days.
             </div>
           </div>
           <div className=" w-full md:w-[60%] h-full flex flex-col justify-start items-center md:items-end gap-8 md:gap-0">
@@ -66,23 +76,13 @@ const Footer = () => {
                   </svg>
                 </span>
                 <span class="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">
-                  
-               
                   apply for homechef
-                 
-                 
                 </span>
-                <span class="relative invisible">
-                  
-              
-               
-                  apply for homechef
-                 
-                  
-                  </span>
+                <span class="relative invisible">apply for homechef</span>
               </a>
             </div>
             <div className="flex md:items-start gap-4 md:absolute md:bottom-0">
+              
               <a href="https://www.instagram.com/fgito_app/">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -106,7 +106,6 @@ const Footer = () => {
                 >
                   <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
                 </svg>
-                
               </a>
             </div>
           </div>
